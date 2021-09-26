da8ff55e60e3d863c2b06001f4a250cb532d1be3
KSanthanesh <santhanesh_2004@yahoo.com>
2021-09-26 19:44:52 +0000

    Add models.py in meals folder

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Meals(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(max_length=500)
+    people = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    preparation_time = models.IntegerField()
+    featured_image = CloudinaryField('image', default='placeholder')
+
+
+    class Meta:
+        verbose_name = 'meal'
+        verbose_name_plural = 'meals'
+
+    def __str__(self):
+        return self.name
+
